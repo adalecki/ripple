77b6fd389e98c5614a77b16e7804b4bdd09b3671
Alex Dalecki <alex.dalecki@stjude.org>
2025-05-21 12:44:45 -0500

    .

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 export default {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom', // Changed from 'node' to 'jsdom' for React component testing
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy' // Mock CSS imports
@@ -10,16 +10,13 @@ export default {
       'ts-jest',
       {
         tsconfig: {
-          // Explicitly set options that were causing issues
           esModuleInterop: true,
-          jsx: 'react-jsx',
-          // Add other necessary options if they arise
+          jsx: 'react-jsx'
         },
       },
     ],
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  // Removed extensionsToTreatAsEsm as it's often problematic with Jest + ts-jest default setup
-  setupFilesAfterEnv: ['@testing-library/jest-dom'], // Changed path for DOM matchers
+  setupFilesAfterEnv: ['@testing-library/jest-dom']
 };
