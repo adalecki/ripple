ef3df1268d512ad64dbd1aac73f7f0c6f6c9ab29
Alex Dalecki <17483756+adalecki@users.noreply.github.com>
2026-09-17 12:02:33 -0500

    changed default split csvs value

--- src/config/changelog.ts
+++ src/config/changelog.ts
@@ -11,41 +11,49 @@ export interface ChangeEntry {
 
 export const CHANGELOG: ChangelogEntry[] = [
   {
+    version: '1.0.2',
+    date: '2026-09-17',
+    changes: [
+      { type: 'Backend', text: 'Fixed Plate Designer bugs selecting wells with different sized plates, and edge case deleting a pattern after changing plate size' },
+      { type: 'Backend', text: 'Changed default option for "Split Output CSVs" to "False"' }
+    ]
+  },
+  {
     version: '1.0.1',
     date: '2026-09-03',
     changes: [
-      {type: 'Backend', text: 'Refactor precalculator class to pull functions into utils file instead of living on class'},
-      {type: 'Backend', text: 'Implemented saving dead volumes on plate object instead of precalculator object'},
-      {type: 'UI', text: 'Plate types configurable in preferences popup'},
-      {type: 'UI', text: 'Preferences reset now only restores selected category to default values, not all preferences'},
-      {type: 'UI', text: 'Added changelog plus alert'}
+      { type: 'Backend', text: 'Refactor precalculator class to pull functions into utils file instead of living on class' },
+      { type: 'Backend', text: 'Implemented saving dead volumes on plate object instead of precalculator object' },
+      { type: 'UI', text: 'Plate types configurable in preferences popup' },
+      { type: 'UI', text: 'Preferences reset now only restores selected category to default values, not all preferences' },
+      { type: 'UI', text: 'Added changelog plus alert' }
     ]
   },
   {
     version: '1.0.0',
     date: '2026-08-25',
     changes: [
-      {type: 'UI', text: 'Added combinations editable from designer interface, changed to Combination-N notation'},
-      {type: 'UI', text: 'Added Cocktail Builder tool for volume-based arbitrary combinations'},
-      {type: 'Backend', text: 'Added endpoint test suite checking final transfer list files against known good copies'},
-      {type: 'Backend', text: 'Fixed bug in intermediate conc generation when solvent limit is very high'}
+      { type: 'UI', text: 'Added combinations editable from designer interface, changed to Combination-N notation' },
+      { type: 'UI', text: 'Added Cocktail Builder tool for volume-based arbitrary combinations' },
+      { type: 'Backend', text: 'Added endpoint test suite checking final transfer list files against known good copies' },
+      { type: 'Backend', text: 'Fixed bug in intermediate conc generation when solvent limit is very high' }
     ]
   },
   {
     version: '0.9.0',
     date: '2026-07-01',
     changes: [
-      {type: 'Backend', text: 'Changed to use HTML Canvas for plate view everywhere'},
-      {type: 'Backend', text: 'Updated README'},
-      {type: 'UI', text: 'Added transfer settings to form in Transfer Calculator'},
-      {type: 'UI', text: 'Import/Export reformat schemes in Plate Reformat'}
+      { type: 'Backend', text: 'Changed to use HTML Canvas for plate view everywhere' },
+      { type: 'Backend', text: 'Updated README' },
+      { type: 'UI', text: 'Added transfer settings to form in Transfer Calculator' },
+      { type: 'UI', text: 'Import/Export reformat schemes in Plate Reformat' }
     ]
   },
   {
     version: '0.8.9',
     date: '2026-04-21',
     changes: [
-      {type: 'UI', text: 'Added destination map download in Transfer Calculator'}
+      { type: 'UI', text: 'Added destination map download in Transfer Calculator' }
     ]
   }
 ];
